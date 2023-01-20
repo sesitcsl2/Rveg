4d5007ef8836d9e7b0dd9256cbebc60c62a66650
Premek <premekkral@seznam.cz>
2023-01-20 10:38:03 +0100

    CRAN sumbit
    CRAN sumbit

--- R/Rveg-package.R
+++ R/Rveg-package.R
@@ -1,3 +1,19 @@
+#' @description Simple and fast tool for transforming phytosociological vegetation data into digital form for the following analysis.
+#' For detailed guide run \code{vignette("Rveg","Rveg")}
+#' or check the quick start guide on <https://github.com/sesitcsl2/Rveg>
+#'
+#' @references
+#' DANIHELKA J., J. CHRTEK a Z. KAPLAN, 2012. Checklist of vascular plants of the czech republic. Preslia. 84(3), 647–811. ISSN 00327786.
+#'
+#' HENNEKENS S.M. a J.H.J. SCHAMIN&#201;E, 2001. TURBOVEG, a comprehensive data base management system for vegetation data. Journal of Vegetation Science. 12(4), 589–591. ISSN 1100-9233. Available at: <doi:10.2307/3237010>
+#'
+#' TICH&#221; L., 2002. JUICE, software for vegetation classification. Journal of Vegetation Science. 13(3), 451. ISSN 1100-9233. Available at: <doi:10.1658/1100-9233(2002)013&#091;0451:jsfvc&#093;2.0.co;2>
+#'
+#' WICKHAM H., R. FRANCOIS, L HENRY, K MULLER, 2022. dplyr: A Grammar of Data Manipulation. R package version 1.0.10. Available at: <https://CRAN.R-project.org/package=dplyr>
+#'
+#'
+#'
+#'
 #' @keywords internal
 "_PACKAGE"
 
@@ -8,3 +24,4 @@
 #' @importFrom utils write.table
 ## usethis namespace: end
 NULL
+
