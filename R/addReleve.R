4eb6370d7a04edadf1a12eed9d6649184459ec10
Premek <premekkral@seznam.cz>
2024-03-25 11:34:56 +0100

    test
    test

--- R/addReleve.R
+++ R/addReleve.R
@@ -128,7 +128,8 @@ addReleve <- function(DATABASE = "NEW", SAVE = "default", checklist = "default",
         ID <- ncol(DATA2)
         colnames(RelNew)[2] <- (ID - 1)
         #Header[1, 2] <- (length(colnames(HeaderDATA2)))
-        Header[1, 2] <- HeaderDATA2[1,ncol(HeaderDATA2)]+1
+        #Header[1, 2] <- HeaderDATA2[1,ncol(HeaderDATA2)]+1
+        printe(HeaderDATA2)
         ab <- readline("DATE?(Y/M/D-2009/07/05) ")
         bb <- readline("SPRINGDATE?(Y/M/D-2009/07/05) ")
         bc <- readline("LOCALITY? ")
